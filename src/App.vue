62afb19162ca8ccb1e454542db51de189fa039f9
Dirga <cekingx@github.com>
2019-03-28 10:33:47 +0800

    cincang header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>|
-      <router-link to="/todo">Todo</router-link>
-    </div>
-    <router-view/>
+  <div class="site-wrap">
+    <app-header/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import Header from "./components/Header/Header";
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: "App",
+  components: {
+    "app-header": Header
+  }
+};
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
+@import "./assets/scss/style";
 </style>
+
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="site-navbar" role="banner">
+    <header-top/>
+    <header-navigation/>
+  </header>
+</template>
+
+<script>
+import HeaderTop from "./Header__top";
+import HeaderNavigation from "./Header__navigation";
+
+export default {
+  name: "Header",
+  components: {
+    "header-top": HeaderTop,
+    "header-navigation": HeaderNavigation
+  }
+};
+</script>
+
