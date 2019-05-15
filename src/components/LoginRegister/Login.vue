5b862f2fad13e9307a867eebcc206f7b9f8ff715
Dirga <cekingx@github.com>
2019-05-15 21:36:32 +0800

    fix tampilan login register

--- src/components/LoginRegister/Login.vue
+++ src/components/LoginRegister/Login.vue
@@ -1,41 +1,40 @@
 <template>
-  <div id="loginbox">
-    <form id="loginform" class="form-vertical" action="index.html">
-      <div class="control-group normal_text">
-        <h3>
-          <img src="http://35.244.219.12/logo.png" alt="Logo">
-        </h3>
-      </div>
-      <div class="control-group">
-        <div class="controls">
-          <div class="main_input_box">
-            <span class="add-on bg_lg">
-              <i class="icon-user"></i>
-            </span>
-            <input type="email" placeholder="Email">
-          </div>
-        </div>
-      </div>
-      <div class="control-group">
-        <div class="controls">
-          <div class="main_input_box">
-            <span class="add-on bg_ly">
-              <i class="icon-lock"></i>
-            </span>
-            <input type="password" placeholder="Password">
-          </div>
-        </div>
-      </div>
-      <div class="form-actions">
-        <span class="pull-left">
-          <a href="register.html" class="flip-link btn btn-info" id="to-recover">Sign Up</a>
-        </span>
-        <span class="pull-right">
-          <a type="submit" class="btn btn-success">Sign In</a>
-        </span>
-      </div>
-    </form>
-  </div>
+  <form class="form-signin">
+    <div class="text-center mb-4">
+      <h1 class="h3 mb-3 font-weight-normal">Sign In</h1>
+    </div>
+
+    <div class="form-label-group">
+      <input
+        type="email"
+        id="inputEmail"
+        class="form-control"
+        placeholder="Email address"
+        required
+        autofocus
+      >
+      <label for="inputEmail">Email address</label>
+    </div>
+
+    <div class="form-label-group">
+      <input
+        type="password"
+        id="inputPassword"
+        class="form-control"
+        placeholder="Password"
+        required
+      >
+      <label for="inputPassword">Password</label>
+    </div>
+
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+
+    <div class="text-center mt-2">
+      <p>or</p>
+    </div>
+
+    <router-link to="/register" class="btn btn-lg btn btn-outline-danger btn-block">Sign Up</router-link>
+  </form>
 </template>
 
 <script>
@@ -44,5 +43,121 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+html,
+body {
+  height: 100%;
+}
+
+body {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 420px;
+  padding: 15px;
+  margin: auto;
+}
+
+.form-label-group {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.form-label-group > input,
+.form-label-group > label {
+  height: 3.125rem;
+  padding: 0.75rem;
+}
+
+.form-label-group > label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  margin-bottom: 0; /* Override default `<label>` margin */
+  line-height: 1.5;
+  color: #495057;
+  pointer-events: none;
+  cursor: text; /* Match the input under the label */
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  transition: all 0.1s ease-in-out;
+}
+
+.form-label-group input::-webkit-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input:-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-moz-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::placeholder {
+  color: transparent;
+}
+
+.form-label-group input:not(:placeholder-shown) {
+  padding-top: 1.25rem;
+  padding-bottom: 0.25rem;
+}
+
+.form-label-group input:not(:placeholder-shown) ~ label {
+  padding-top: 0.25rem;
+  padding-bottom: 0.25rem;
+  font-size: 12px;
+  color: #777;
+}
+
+/* Fallback for Edge
+-------------------------------------------------- */
+@supports (-ms-ime-align: auto) {
+  .form-label-group > label {
+    display: none;
+  }
+  .form-label-group input::-ms-input-placeholder {
+    color: #777;
+  }
+}
+
+/* Fallback for IE
+-------------------------------------------------- */
+@media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
+  .form-label-group > label {
+    display: none;
+  }
+  .form-label-group input:-ms-input-placeholder {
+    color: #777;
+  }
+}
+.bd-placeholder-img {
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
 </style>
